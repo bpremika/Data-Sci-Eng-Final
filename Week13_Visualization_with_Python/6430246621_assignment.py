f5b498381e6378a2afaf1e042ea1ff43cefa6d69
bpremika <premika.yu@gmail.com>
2024-04-23 10:46:06 +0700

    feat: add train random forest with no param

--- Week13_Visualization_with_Python/6430246621_assignment.py
+++ Week13_Visualization_with_Python/6430246621_assignment.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import folium
-from folium.plugins import MarkerCluster
-from streamlit_folium import st_folium
 import pydeck as pdk
 
 path = 'RainDaily_Tabular.csv'
